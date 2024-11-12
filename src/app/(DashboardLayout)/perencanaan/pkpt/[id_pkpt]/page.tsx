cbc746448a051bf1cc61145b35aba1bb79c944fc
rikzafauzannf <rikzarazor491@gmail.com>
2024-11-12 15:38:39 +0700

    update minor view pkpt for next get data and change output data

--- src/app/(DashboardLayout)/perencanaan/pkpt/[id_pkpt]/page.tsx
+++ src/app/(DashboardLayout)/perencanaan/pkpt/[id_pkpt]/page.tsx
@@ -1,101 +1,119 @@
-import { CardComponents, CardHeaderContent } from '@/app/components/Global/Card'
-import { InputFiledComponent } from '@/app/components/Global/Input'
+import {
+  CardComponents,
+  CardHeaderContent,
+} from '@/app/components/Global/Card';
+import { InputFiledComponent } from '@/app/components/Global/Input';
+import { NotofikasiEdit } from '@/app/components/Global/Notif';
 
 // Cara 1: Menggunakan props params
 interface PageProps {
   params: {
-    id_pkpt: string
-  }
+    id_pkpt: string;
+  };
 }
 
 const ViewPkptPage = ({ params }: PageProps) => {
   // Sekarang kita bisa menggunakan params.id_pkpt
-  console.log('ID PKPT:', params.id_pkpt)
-    const id_pkpt = params.id_pkpt;
+  console.log('ID PKPT:', params.id_pkpt);
+  const id_pkpt = params.id_pkpt;
+
+  const dummyDummyNOtifikasi = [
+    {
+      username:"reza",
+      date:"12-11-2024 - 14.50 wib"
+    },
+    {
+      username:"reza",
+      date:"12-11-2024 - 14.50 wib"
+    },
+    {
+      username:"reza",
+      date:"12-11-2024 - 14.50 wib"
+    },
+    {
+      username:"reza",
+      date:"12-11-2024 - 14.50 wib"
+    },
+    {
+      username:"reza",
+      date:"12-11-2024 - 14.50 wib"
+    },
+  ]
   return (
-    <div className='space-y-3'>
-    
-    <section className='flex justify-between items-center'>
-    <h3>Lihat PKPT ({id_pkpt})</h3>
-    <div className='space-x-3'>
-        <button className='py-1 px-2 bg-slate-300 rounded-md shadow-md'>Hapus</button>
-        <button className='py-1 px-2 bg-slate-300 rounded-md shadow-md'>Edit</button>
-    </div>
-    </section>
-    
-    {/* persiapan data */}
-    <section className='grid grid-cols-3 gap-3'>
-    <div className='col-span-2 space-y-3'>
-    <CardComponents>
-        <section className="grid md:grid-cols-2 w-full gap-3">
-          <InputFiledComponent
-            label="Jenis Pengawasan"
-            identiti="jpengawasan"
-            type="text"
-            name="JenisPengawasan"
-            placeholder="Masukan Kategori Jenis Pengawasan"
-          />
-          <InputFiledComponent
-            label="Area Pengawasan"
-            identiti="area"
-            type="text"
-            name="AreaPengawasan"
-            placeholder="Masukan Area Pengawasan"
-          />
-          <InputFiledComponent
-            label="Ruang Lingkup"
-            identiti="rLingkup"
-            type="text"
-            name="RuangLingkup"
-            placeholder="Masukan Ruang Lingkup Pengawasan"
-          />
-          <InputFiledComponent
-            label="Tujuan / Sasaran"
-            identiti="tSasaran"
-            type="text"
-            name="TujuanSasaran"
-            placeholder="Masukan Tujuan / Sasaran pengawasan"
-          />
-        </section>
-      </CardComponents>
-      {/* penjadwalan */}
-      <CardComponents>
-        <h3>Jadwal Pengawasan</h3>
-        <section className="grid md:grid-cols-2 gap-3">
-          <InputFiledComponent
-            label="Rencana Penugasan"
-            identiti="rPenugasan"
-            type="date"
-            name="RencanaPenugasan"
-            placeholder="Tentukan rencana penugasan"
-          />
-          <InputFiledComponent
-            label="Rencana Penerbitan"
-            identiti="rPenerbitan"
-            type="date"
-            name="RencanaPenerbitan"
-            placeholder="Tentukan rencana Penerbitan"
-          />
-        </section>
-      </CardComponents>
-    </div>
-      {/* notif */}
-      <CardComponents>
-        <h3>Perubahan data oleh</h3>
-        <div className='max-h-[100%] overflow-y-scroll space-y-3'>
-            <div className='p-2 bg-slate-300/50 rounded-md shadow-md'>
-              <div className='flex space-x-3 items-center'>
-                <div className='w-[60px] h-[60px] rounded-full shadow'></div>
-                <div>
-                <h3>Reza</h3>
-                <small className='text-end text-slate-800'>19:00 wib. 12-11-2024</small>
-                </div>
-              </div>
-            </div>            
+    <div className="space-y-3">
+      <section className="flex justify-between items-center">
+        <h3>Lihat PKPT ({id_pkpt})</h3>
+        {/* tombol action */}
+        <div className="space-x-3">
+          <button className="py-1 px-2 bg-slate-300 rounded-md shadow-md">
+            Hapus
+          </button>
+          <button className="py-1 px-2 bg-slate-300 rounded-md shadow-md">
+            Edit
+          </button>
         </div>
-      </CardComponents>
-    </section>
-      
+      </section>
+
+      {/* persiapan data */}
+      <section className="grid grid-cols-3 gap-3">
+        <div className="col-span-2 space-y-3">
+          <CardComponents>
+            <section className="grid md:grid-cols-2 w-full gap-3">
+              <InputFiledComponent
+                label="Jenis Pengawasan"
+                identiti="jpengawasan"
+                type="text"
+                name="JenisPengawasan"
+                placeholder="Masukan Kategori Jenis Pengawasan"
+              />
+              <InputFiledComponent
+                label="Area Pengawasan"
+                identiti="area"
+                type="text"
+                name="AreaPengawasan"
+                placeholder="Masukan Area Pengawasan"
+              />
+              <InputFiledComponent
+                label="Ruang Lingkup"
+                identiti="rLingkup"
+                type="text"
+                name="RuangLingkup"
+                placeholder="Masukan Ruang Lingkup Pengawasan"
+              />
+              <InputFiledComponent
+                label="Tujuan / Sasaran"
+                identiti="tSasaran"
+                type="text"
+                name="TujuanSasaran"
+                placeholder="Masukan Tujuan / Sasaran pengawasan"
+              />
+            </section>
+          </CardComponents>
+          {/* penjadwalan */}
+          <CardComponents>
+            <h3>Jadwal Pengawasan</h3>
+            <section className="grid md:grid-cols-2 gap-3">
+              <InputFiledComponent
+                label="Rencana Penugasan"
+                identiti="rPenugasan"
+                type="date"
+                name="RencanaPenugasan"
+                placeholder="Tentukan rencana penugasan"
+              />
+              <InputFiledComponent
+                label="Rencana Penerbitan"
+                identiti="rPenerbitan"
+                type="date"
+                name="RencanaPenerbitan"
+                placeholder="Tentukan rencana Penerbitan"
+              />
+            </section>
+          </CardComponents>
+        </div>
+        {/* notif */}
+        <NotofikasiEdit data={dummyDummyNOtifikasi}/>
+      </section>
+
       {/* Hari Penugasan */}
       <CardComponents>
         <h3>Hari Penugasan</h3>
@@ -182,7 +200,7 @@ const ViewPkptPage = ({ params }: PageProps) => {
         </section>
       </CardComponents>
     </div>
-  )
-}
+  );
+};
 
-export default ViewPkptPage
+export default ViewPkptPage;
---
+++ src/app/components/Global/Notif.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CardComponents } from './Card';
+
+interface DetailsProps {
+  username: string;
+  date: string;
+}
+
+interface Props {
+  data: DetailsProps[];
+}
+
+export const NotofikasiEdit: React.FC<Props> = ({ data }) => {
+  return (
+    <CardComponents>
+      <h3>Perubahan data oleh</h3>
+      <div className="max-h-[250px] overflow-y-scroll space-y-3">
+        {/* mapping data */}
+        {data.map((items, index) => (
+          <div className="p-2 bg-slate-300/50 rounded-md shadow-md" key={index}>
+            <div className="flex space-x-3 items-center">
+              <div className="w-[60px] h-[60px] rounded-full shadow"></div>
+              <div>
+                <h3>{items.username}</h3>
+                <small className="text-end text-slate-800">
+                  {items.date}
+                </small>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </CardComponents>
+  );
+};
