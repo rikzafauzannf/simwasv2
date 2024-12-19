102ce4f7aea34e618b6ca35c31fdcc806ad0cbb9
rikzafauzannf <rikzarazor491@gmail.com>
2024-12-19 10:37:33 +0700

    minor

--- src/app/dashboard/perencanaan/pkpt/page.tsx
+++ src/app/dashboard/perencanaan/pkpt/page.tsx
@@ -1,8 +1,6 @@
 import { ButtonLinkComponent } from '@/app/components/Global/Button';
 import { CardComponents } from '@/app/components/Global/Card';
 import AkumulasiDataPKPT from '@/app/components/perencanaan/AkumulasiDataPKPT';
-import InputNonPKPT from '@/app/components/perencanaan/form/InputNonPKPT';
-import PKPTDataTable from '@/app/components/perencanaan/table/PKPTDataTable';
 import TablePKPT from '@/app/components/perencanaan/table/TablePKPT';
 import React from 'react';
 
@@ -21,7 +19,6 @@ const PkptPage = () => {
           <TablePKPT />
         </CardComponents>        
       </div>
-      <PKPTDataTable/>
     </div>
   );
 };
