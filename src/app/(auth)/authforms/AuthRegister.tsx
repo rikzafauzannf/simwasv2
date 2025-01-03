ff8a9ad41b34ad33ac516e4ef10e332dd0c939ed
rikzafauzannf <rikzarazor491@gmail.com>
2025-01-03 14:53:18 +0700

    minor

--- src/app/(auth)/authforms/AuthRegister.tsx
+++ src/app/(auth)/authforms/AuthRegister.tsx
@@ -105,7 +105,7 @@ const AuthRegister = () => {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className="grid gap-2">
-        <h3 className="text-2xl">Daftar</h3>
+        {/* <h3 className="text-2xl">Daftar</h3> */}
         <section className="grid grid-cols-2 gap-2 w-full">
           <div className="col-span-2">
             <InputFieldComponent
