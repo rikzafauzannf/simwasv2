569bc46c06bd3d4fa5eb328b155cb62d118c3e38
rikzafauzannf <rikzarazor491@gmail.com>
2025-01-17 06:37:49 +0700

    up detail surat tugas

--- src/app/dashboard/perencanaan/surattugas/[id_st]/page.tsx
+++ src/app/dashboard/perencanaan/surattugas/[id_st]/page.tsx
@@ -5,8 +5,13 @@ import { Document, Page } from '@react-pdf/renderer';
 import { useFetchById } from '@/hooks/useFetchById';
 import { SuratTugasData } from '@/interface/interfaceSuratTugas';
 import { CardComponents } from '@/app/components/Global/Card';
-import { useGetNameUser } from '@/hooks/useGetName';
+import { useGetNameJenisAudit, useGetNameUser } from '@/hooks/useGetName';
 import AuthRoleWrapper from '@/middleware/HOC/withRoleWrapper';
+import Link from 'next/link';
+import { LHPData, NHPData } from '@/interface/interfaceHasilPengawasan';
+import { useFetchAll } from '@/hooks/useFetchAll';
+import { Icon } from '@iconify/react/dist/iconify.js';
+import { formatToLocalDate } from '@/data/formatData';
 
 interface PageProps {
   params: {
@@ -22,7 +27,13 @@ const ViewSuratTugas: React.FC<PageProps> = ({ params }) => {
     id
   );
 
-  const { getNameUser, getUserNIP } = useGetNameUser();
+  const { data: DataNHP } = useFetchAll<NHPData>('nhp');
+  const { data: DataLHP } = useFetchAll<LHPData>('lhp');
+
+  const NHPMAP = DataNHP.filter((item) => item.id_st === Number(id));
+
+  const { getNameUser, getUserNIP, getUserPhone } = useGetNameUser();
+  const { getNameJenisAudit } = useGetNameJenisAudit();
 
   // URL PDF yang sesuai dengan id_st
   const pdfUrl = DataST?.link_st;
@@ -43,11 +54,11 @@ const ViewSuratTugas: React.FC<PageProps> = ({ params }) => {
       allowedRoles={['Admin', 'Perencana', 'Pimpinan', 'Pelaksana', 'Auditor']}
     >
       <h3 className="text-xl mb-3"># Detail Surat Tugas</h3>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-3 gap-3 mb-3">
         <div className="col-span-2 space-y-3">
           <CardComponents>
             {/* <h2 className='text-lg'>Detail ST</h2> */}
-            <section className='grid grid-cols-3 gap-3 w-full'>
+            <section className="grid grid-cols-3 gap-3 w-full">
               <div>
                 <p>Bulan</p>
                 <h3>{DataST?.bulan}</h3>
@@ -69,7 +80,7 @@ const ViewSuratTugas: React.FC<PageProps> = ({ params }) => {
                 <p>Jumlah HP</p>
                 <h3>{DataST?.jumlah_hp} - Hari</h3>
               </div>
-              <div className='col-span-2'>
+              <div className="col-span-2">
                 <p>Waktu Penugasan Penugasan</p>
                 <h3>{DataST?.waktu_penugasan}</h3>
               </div>
@@ -77,7 +88,7 @@ const ViewSuratTugas: React.FC<PageProps> = ({ params }) => {
           </CardComponents>
 
           <CardComponents>
-          <div className="grid md:grid-cols-2 gap-3">
+            <div className="grid md:grid-cols-2 gap-3">
               <div>
                 <p>Tim Pemeriksa</p>
                 <h2>{getNameUser(Number(DataST?.tim_pemeriksa))}</h2>
@@ -99,7 +110,7 @@ const ViewSuratTugas: React.FC<PageProps> = ({ params }) => {
                 <p>NIP - {getUserNIP(Number(DataST?.ketua_tim))}</p>
               </div>
               <div className="md:col-span-2">
-                <hr />
+                <hr className="my-3" />
                 <p>Anggota Tim</p>
                 <div className="grid md:grid-cols-3 gap-3">
                   {DataST?.anggota_tim.split(',').map((id) => {
@@ -116,58 +127,37 @@ const ViewSuratTugas: React.FC<PageProps> = ({ params }) => {
           </CardComponents>
 
           <CardComponents>
-            <div className="grid md:flex md:justify-between md:items-center gap-2 w-full">
+            <div className="grid grid-cols-3 gap-3">
               <div>
-                <p>{DataST?.bulan}</p>
-                <p className="text-slate-800 font-semibold">
-                  {DataST?.waktu_penugasan}
-                </p>
-              </div>
-
-              <h1>{DataST?.no_tglsp}</h1>
-              <p>Laporan - {DataST?.jumlah_laporan}</p>
-              <p>Object - {DataST?.jumlah_objek}</p>
-            </div>
-
-            <hr />
-            <h2># {DataST?.program_audit}</h2>
-            <h3>{DataST?.keterangan}</h3>
-            <hr />
-
-            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-3">
-              <div>
-                <p>Tim Pemeriksa</p>
-                <h2>{getNameUser(Number(DataST?.tim_pemeriksa))}</h2>
-                <p>NIP - {getUserNIP(Number(DataST?.tim_pemeriksa))}</p>
+                <p>Jumlah Objek Pengawasan</p>
+                <h3>{DataST?.jumlah_objek}</h3>
               </div>
               <div>
-                <p>Wakil Penanggung Jawab</p>
-                <h2>{getNameUser(Number(DataST?.wk_penanggung_jawab))}</h2>
-                <p>NIP - {getUserNIP(Number(DataST?.wk_penanggung_jawab))}</p>
+                <p>Jumlah Laporan</p>
+                <h3>{DataST?.jumlah_laporan}</h3>
               </div>
               <div>
-                <p>Pengendali Teknis</p>
-                <h2>{getNameUser(Number(DataST?.pengendali_teknis))}</h2>
-                <p>NIP - {getUserNIP(Number(DataST?.pengendali_teknis))}</p>
+                <p>No.Tgl.LHP/LHE/LHR</p>
+                <h3>{DataST?.no_tgllh}</h3>
               </div>
               <div>
-                <p>Ketua Tim</p>
-                <h2>{getNameUser(Number(DataST?.ketua_tim))}</h2>
-                <p>NIP - {getUserNIP(Number(DataST?.ketua_tim))}</p>
+                <p>Jenis Audit</p>
+                <h3>{getNameJenisAudit(Number(DataST?.id_jenis_audit))}</h3>
               </div>
-              <div className="md:col-span-2 lg:col-span-4">
-                <hr />
-                <p>Anggota Tim</p>
-                <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-3">
-                  {DataST?.anggota_tim.split(',').map((id) => {
-                    return (
-                      <div>
-                        <h2>{getNameUser(Number(id))}</h2>
-                        <p>NIP - {getUserNIP(Number(id))}</p>
-                      </div>
-                    );
-                  })}
-                </div>
+              <div>
+                <p>Keterangan</p>
+                <h3>{DataST?.keterangan}</h3>
+              </div>
+              <div>
+                <p>Link ST</p>
+                <Link
+                  href={String(DataST?.link_st)}
+                  target="blank"
+                  className="text-blue-400 hover:text-blue-600 font-semibold"
+                >
+                  Akses Link ST
+                </Link>
+                {/* <h3>{DataST?.link_st}</h3> */}
               </div>
             </div>
           </CardComponents>
@@ -180,6 +170,74 @@ const ViewSuratTugas: React.FC<PageProps> = ({ params }) => {
           />
         </CardComponents>
       </div>
+      <CardComponents>
+        <h2 className="font-bold text-gray-800 mb-4">
+          Daftar NHP - {NHPMAP.length}
+        </h2>
+        <ul className="list-disc list-inside space-y-3">
+          {NHPMAP.map((item, index) => {
+            return (
+              <li
+                className="text-gray-700 hover:text-gray-900 transition shadow"
+                key={index}
+              >
+                <small>{formatToLocalDate(item.created_at)}</small>
+                <div className="flex justify-between items-center gap-3">
+                  <div className="flex-1 flex gap-2">
+                    <Link href={item.file_nhp} target="blank">
+                      <Icon
+                        icon="solar:paperclip-line-duotone"
+                        width="16"
+                        height="16"
+                      />
+                    </Link>
+                    <h3>{item.keterangan_nhp}</h3>
+                  </div>
+                  <Link href={String(getUserPhone(item.id_user))}>
+                    <p>
+                      {getNameUser(item.id_user)} - {getUserNIP(item.id_user)}
+                    </p>
+                  </Link>
+                </div>
+                <ul className="list-disc pl-6 space-y-1 mt-2">
+                  {DataLHP.filter(
+                    (itemsLHP) => itemsLHP.id_nhp === item.id_nhp
+                  ).map((item, index) => (
+                    <li
+                      key={index}
+                      className="text-gray-600 hover:text-gray-800 transition"
+                    >
+                      <small>{formatToLocalDate(item.created_at)}</small>
+                      <div className="flex justify-between items-center gap-3">
+                        <div className="flex-1 flex gap-2">
+                          <Link href={item.file_lhp} target="blank">
+                            <Icon
+                              icon="solar:paperclip-line-duotone"
+                              width="16"
+                              height="16"
+                            />
+                          </Link>
+                          <h3>{item.keterangan_lhp}</h3>
+                        </div>
+                        <Link href={String(getUserPhone(item.id_user))}>
+                          <p>
+                            {getNameUser(item.id_user)} -{' '}
+                            {getUserNIP(item.id_user)}
+                          </p>
+                        </Link>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            );
+          })}
+
+          {/* <li className="text-gray-700 hover:text-gray-900 transition">Item 2</li>
+    <li className="text-gray-700 hover:text-gray-900 transition">Item 3</li>
+    <li className="text-gray-700 hover:text-gray-900 transition">Item 4</li> */}
+        </ul>
+      </CardComponents>
     </AuthRoleWrapper>
   );
 };
