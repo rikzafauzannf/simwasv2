e6ce6886e660ddd5617e46ca26db2085ade62c08
rikzafauzannf <rikzarazor491@gmail.com>
2025-05-05 18:56:43 +0700

    test loading lottie file

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lottie from "lottie-react";
+import animationsload from '../../../public/animation_load.json'
+const Loading = () => {
+  return (
+    <div className='w-full min-h-dvh flex  justify-center items-center'>
+      <Lottie animationData={animationsload} loop={true}/>
+    </div>
+  )
+}
+
+export default Loading
